cf0be80b85977bb107dfcfccec978531826eeb33
tayeb <aviata@campus.technion.ac.il>
2021-12-06 15:21:44 +0200

    continue with player

--- src/Card.java
+++ src/Card.java
@@ -10,29 +10,29 @@ public class Card {
     public int getNumber(){
         return number;
     }
-    public String getSuit{
+    public String getSuit(){
         return suit;
     }
 
     public int compare(Card other){
-        if(number < other)
+        if(number < other.number)
             return  -1;
-        if(number == other)
+        if(number == other.number)
             return 0;
-        if(number > other)
-            return  1;
+
+        return  1;
     }
 
     public String toString(){
         if(number == 1)
-            System.out.println("Ace of " + suit);
+            return ("Ace of " + suit);
         else if(number == 11)
-            System.out.println("Jack of " + suit);
+            return ("Jack of " + suit);
         else if(number == 12)
-            System.out.println("Queen of " + suit);
+            return ("Queen of " + suit);
         else if(number == 13)
-            System.out.println("King of " + suit);
-        else
-            System.out.println("Ace of " + suit);
+            return ("King of " + suit);
+
+        return (number + " of " + suit);
     }
 }
---
+++ src/Deck.java
@@ -0,0 +1,43 @@
+import java.util.Random;
+
+public class Deck {
+    private Card[] currentDeckArray = new Card[52];
+    private String[] suitArray = {"SPADES", "DIAMONDS", "CLUBS", "HEARTS"};
+    private int numberOfCards = 0;
+
+    public Deck(boolean answer){
+        int counter = 0;
+        if(answer) {
+            while (counter < 52) {
+                for (String currentSuit : suitArray)
+                    for (int j = 1; j < 14; j++) {
+                        currentDeckArray[counter] = new Card(j, currentSuit);
+                        counter++;
+                    }
+            }
+            numberOfCards = counter+1;
+        }
+    }
+
+    public void addCard(Card card){
+        currentDeckArray[numberOfCards] = card;
+    }
+
+    Card removeTopCard(){
+        Card cardToRemove = currentDeckArray[numberOfCards - 1];
+        currentDeckArray[numberOfCards - 1] = null;
+        return cardToRemove;
+    }
+
+    boolean isEmpty(){
+        if(numberOfCards == 0)
+            return true;
+        else
+            return false;
+    }
+   /* Random newRnd = new Main.rnd;
+    void Shuffle(){
+        for(int i = 0; i < numberOfCards; i++)
+            int next = newRnd.nextInt(numberOfCards - 1);
+    }*/
+}
